676494119230e00d1f0cc0c01db3da2742ce5ec7
Allison Andric da Silva <allison.andric.silva@gmail.com>
2025-09-14 03:02:35 +0000

    initial commit

---
+++ src/main/java/br/com/gerenciamento/clientes/carros/backend/SpringBootApplication.java
@@ -0,0 +1,4 @@
+package br.com.gerenciamento.clientes.carros.backend;
+
+public @interface SpringBootApplication {
+}
---
+++ src/test/java/br/com/gerenciamento/clientes/carros/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciamento.clientes.carros.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
